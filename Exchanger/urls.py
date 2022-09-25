4a06252433b4f506388c45953738d44ae08ee159
Mohamed beirouk <mbeirouck@gmail.com>
2022-09-25 19:38:47 +0000

    vvc

--- Exchanger/urls.py
+++ Exchanger/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    #test
 ]
