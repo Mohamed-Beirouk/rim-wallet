f0fe610af179edbfca1d12fa26d500e91c500d23
Mohamed beirouk <mbeirouck@gmail.com>
2022-09-25 19:30:14 +0000

    vv

--- Exchanger/settings.py
+++ Exchanger/settings.py
@@ -54,6 +54,8 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
+    
+    
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
